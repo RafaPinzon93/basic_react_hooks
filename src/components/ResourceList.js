c6fae2cecf2749810b8f91efbfb57c8647e03ddf
RafaPinzon93 <rafapinzon_@hotmail.com>
2019-05-16 08:55:29 -0500

    Added class component for managing resoure requests.

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,29 @@
 import React from "react";
+import axios from "axios";
 
 class ResourceList extends React.Component {
+    state = { resources: [] };
+
+    componentDidMount() {
+        this.getResponse();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.resource !== this.props.resource) {
+            this.getResponse();
+        }
+    }
+
+    async getResponse() {
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/${this.props.resource}`
+        );
+
+        this.setState({ resources: response.data });
+    }
+
     render() {
-        return <div>{this.props.resource}</div>;
+        return <div>{this.state.resources.length}</div>;
     }
 }
 
